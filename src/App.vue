c339a30bbe5d061ab2a6a306c55fef8da3029aa1
Andrey Pitunov <andreypitunov@Andreys-MacBook-Pro-2.local>
2018-02-10 18:56:06 +0300

    load file

---
+++ src/App.vue
@@ -0,0 +1,266 @@
+<template>
+  <div id="app" class="app">
+    <section class="section section_contact contact">
+      <div class="contact__photo">
+        <img src="/dist/photo.png" alt="Andrey Pitunov">
+      </div>
+      <div class="contact__name">Andrey Pitunov</div>
+      <div class="contact__social">
+        <ul class="contact__list">
+          <li class="contact__item">
+            <a href="mailto:andreypitunov@gmail.com">
+              <i class="contact__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 14 14" style="enable-background:new 0 0 14 14;" xml:space="preserve">
+                  <g>
+                    <g>
+                      <path style="" d="M7,9L5.268,7.484l-4.952,4.245C0.496,11.896,0.739,12,1.007,12h11.986    c0.267,0,0.509-0.104,0.688-0.271L8.732,7.484L7,9z"/>
+                      <path style="" d="M13.684,2.271C13.504,2.103,13.262,2,12.993,2H1.007C0.74,2,0.498,2.104,0.318,2.273L7,8    L13.684,2.271z"/>
+                      <polygon style="" points="0,2.878 0,11.186 4.833,7.079   "/>
+                      <polygon style="" points="9.167,7.079 14,11.186 14,2.875   "/>
+                    </g>
+                  </g>
+                </svg>
+              </i>
+            </a>
+          </li>
+          <li class="contact__item">
+            <a href="https://github.com/AndreyPitunov">
+              <i class="contact__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 438.549 438.549" style="enable-background:new 0 0 438.549 438.549;" xml:space="preserve">
+                  <g>
+                    <path d="M409.132,114.573c-19.608-33.596-46.205-60.194-79.798-79.8C295.736,15.166,259.057,5.365,219.271,5.365   c-39.781,0-76.472,9.804-110.063,29.408c-33.596,19.605-60.192,46.204-79.8,79.8C9.803,148.168,0,184.854,0,224.63   c0,47.78,13.94,90.745,41.827,128.906c27.884,38.164,63.906,64.572,108.063,79.227c5.14,0.954,8.945,0.283,11.419-1.996   c2.475-2.282,3.711-5.14,3.711-8.562c0-0.571-0.049-5.708-0.144-15.417c-0.098-9.709-0.144-18.179-0.144-25.406l-6.567,1.136   c-4.187,0.767-9.469,1.092-15.846,1c-6.374-0.089-12.991-0.757-19.842-1.999c-6.854-1.231-13.229-4.086-19.13-8.559   c-5.898-4.473-10.085-10.328-12.56-17.556l-2.855-6.57c-1.903-4.374-4.899-9.233-8.992-14.559   c-4.093-5.331-8.232-8.945-12.419-10.848l-1.999-1.431c-1.332-0.951-2.568-2.098-3.711-3.429c-1.142-1.331-1.997-2.663-2.568-3.997   c-0.572-1.335-0.098-2.43,1.427-3.289c1.525-0.859,4.281-1.276,8.28-1.276l5.708,0.853c3.807,0.763,8.516,3.042,14.133,6.851   c5.614,3.806,10.229,8.754,13.846,14.842c4.38,7.806,9.657,13.754,15.846,17.847c6.184,4.093,12.419,6.136,18.699,6.136   c6.28,0,11.704-0.476,16.274-1.423c4.565-0.952,8.848-2.383,12.847-4.285c1.713-12.758,6.377-22.559,13.988-29.41   c-10.848-1.14-20.601-2.857-29.264-5.14c-8.658-2.286-17.605-5.996-26.835-11.14c-9.235-5.137-16.896-11.516-22.985-19.126   c-6.09-7.614-11.088-17.61-14.987-29.979c-3.901-12.374-5.852-26.648-5.852-42.826c0-23.035,7.52-42.637,22.557-58.817   c-7.044-17.318-6.379-36.732,1.997-58.24c5.52-1.715,13.706-0.428,24.554,3.853c10.85,4.283,18.794,7.952,23.84,10.994   c5.046,3.041,9.089,5.618,12.135,7.708c17.705-4.947,35.976-7.421,54.818-7.421s37.117,2.474,54.823,7.421l10.849-6.849   c7.419-4.57,16.18-8.758,26.262-12.565c10.088-3.805,17.802-4.853,23.134-3.138c8.562,21.509,9.325,40.922,2.279,58.24   c15.036,16.18,22.559,35.787,22.559,58.817c0,16.178-1.958,30.497-5.853,42.966c-3.9,12.471-8.941,22.457-15.125,29.979   c-6.191,7.521-13.901,13.85-23.131,18.986c-9.232,5.14-18.182,8.85-26.84,11.136c-8.662,2.286-18.415,4.004-29.263,5.146   c9.894,8.562,14.842,22.077,14.842,40.539v60.237c0,3.422,1.19,6.279,3.572,8.562c2.379,2.279,6.136,2.95,11.276,1.995   c44.163-14.653,80.185-41.062,108.068-79.226c27.88-38.161,41.825-81.126,41.825-128.906   C438.536,184.851,428.728,148.168,409.132,114.573z"/>
+                  </g>
+                </svg>
+              </i>
+            </a>
+          </li>
+          <li class="contact__item">
+            <a href="https://t.me/AndreyPitunov">
+              <i class="contact__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 300 300" xml:space="preserve">
+                  <g id="XMLID_496_">
+                    <path id="XMLID_497_" d="M5.299,144.645l69.126,25.8l26.756,86.047c1.712,5.511,8.451,7.548,12.924,3.891l38.532-31.412   c4.039-3.291,9.792-3.455,14.013-0.391l69.498,50.457c4.785,3.478,11.564,0.856,12.764-4.926L299.823,29.22   c1.31-6.316-4.896-11.585-10.91-9.259L5.218,129.402C-1.783,132.102-1.722,142.014,5.299,144.645z M96.869,156.711l135.098-83.207   c2.428-1.491,4.926,1.792,2.841,3.726L123.313,180.87c-3.919,3.648-6.447,8.53-7.163,13.829l-3.798,28.146   c-0.503,3.758-5.782,4.131-6.819,0.494l-14.607-51.325C89.253,166.16,91.691,159.907,96.869,156.711z"/>
+                  </g>
+                </svg>
+              </i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section class="section section_info info">
+      <div class="info__title">Front-end Developer</div>
+      <div class="info__discription">
+        Currently front-end developer at <a class="info__link" href="http://12devs.com">@12devs</a>. Also crafting front-end & design for digital products – exclusively for founders and startups.
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+// @import "style/normalize";
+
+svg:not(:root) {
+    overflow: hidden;
+}
+
+*,
+*::after,
+*::before {
+	box-sizing: border-box;
+}
+
+html {
+	font-size: 62.5%;
+}
+
+body {
+	font: 400 1.4rem/1.4 'Whitney-Book', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  margin: 0;
+  background: #1D1E22;
+  height: 100vh;
+}
+
+a {
+	text-decoration: none;
+	outline: none;
+}
+
+.app {
+  display: flex;
+  height: 100%;
+}
+
+.section {
+  display: flex;
+
+  &_contact {
+    width: 10rem;
+  }
+
+  &_info {
+    padding: 3rem 1rem;
+  }
+}
+
+.contact {
+  padding: 3rem 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    & {
+      padding-top: 5.5rem;
+    }
+  }
+
+  @media screen and (max-width: 550px) {
+      & {
+        width: 8rem;
+      }
+    }
+
+  &__photo {
+    width: 6.4rem;
+    height: 6.4rem;
+    border-radius: 50%;
+    overflow: hidden;
+    margin-bottom: 2rem;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  &__name {
+    display: flex;
+    transform: rotate(90deg);
+    padding: 6rem 0;
+    white-space: nowrap;
+    color: #fdfdfd;
+    // font-weight: 600;
+    text-transform: uppercase;
+  }
+
+  &__social {
+    flex: 1;
+    display: flex;
+    align-items: flex-end;
+  }
+
+  &__list {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+
+  &__item {
+    margin: 2rem 0;
+    cursor: pointer;
+  }
+
+  &__icon {
+    width: 2rem;
+    height: 2rem;
+    display: block;
+    fill: #fdfdfd;
+
+    @media screen and (max-width: 768px) {
+      & {
+        width: 2.5rem;
+        height: 2.5rem;
+      }
+    }
+    
+    &:hover {
+      fill: #8C8E98;
+    }
+  }
+}
+
+.info {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 10rem;
+  padding-right: 4rem;
+
+  @media screen and (max-width: 768px) {
+    & {
+      padding-left: 5rem;
+      padding-right: 2rem;
+      justify-content: flex-start;
+      padding-top: 5rem;
+    }
+  }
+
+  @media screen and (max-width: 550px) {
+    & {
+      padding-left: 2rem;
+    }
+  }
+
+  &__title {
+    font-size: 4rem;
+    line-height: 4rem;
+    font-weight: 600;
+    color: #fdfdfd;
+    margin: 2rem 0;
+
+    @media screen and (max-width: 768px) {
+      & {
+        font-size: 3.5rem;
+      }
+    }
+  }
+
+  &__discription {
+    font-size: 2rem;
+    color: #8C8E98;
+    width: 70%;
+
+    @media screen and (max-width: 768px) {
+      & {
+        font-size: 2.5rem;
+        width: 100%;
+      }
+    }
+  }
+
+  &__link {
+    color: #fdfdfd;
+    position: relative;
+
+    &::before {
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: .1rem;
+      background: #fdfdfd;
+      bottom: -.2rem;
+      opacity: .5;
+      transition: all .25s cubic-bezier(0.26, 0.01, 1, 1);
+    }
+
+    &:hover {
+      &::before {
+      opacity: 1;
+    }
+    }
+  }
+}
+</style>
