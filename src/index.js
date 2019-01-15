be634209fca1540adba83595ede782405c99ffc4
Andrey Pitunov <andreypitunov@gmail.com>
2019-01-15 15:50:36 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Resources/style/App.css';
 import App from './Components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
